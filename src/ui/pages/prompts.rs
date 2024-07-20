cec71154b516fe052dae3cec5c3461e40391c768
Wbert Castro <wbert.castro@genius.com>
2024-07-20 14:48:19 -0500

    Stage 2 step 2 completed

--- src/ui/pages/prompts.rs
+++ src/ui/pages/prompts.rs
@@ -24,21 +24,58 @@ impl Prompts {
 }
 
 fn create_epic_prompt() -> Epic {
-    todo!();
+    println!("----------------------------");
+    println!("Epic Name:");
+    let name = get_user_input();
+    println!("Epic Description:");
+    let description = get_user_input();
+
+    Epic {
+        name,
+        description,
+        status: Status::Open,
+        stories: vec![],
+    }
 }
 
 fn create_story_prompt() -> Story {
-    todo!();
+    println!("----------------------------");
+    println!("Story Name:");
+    let name = get_user_input();
+    println!("Story Description:");
+    let description = get_user_input();
+
+    Story {
+        name,
+        description,
+        status: Status::Open,
+    }
 }
 
 fn delete_epic_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!("Are you sure you want to delete this epic? All stories in this epic will also be deleted [Y/n]:");
+    let confirm = get_user_input();
+    confirm == "Y"
 }
 
 fn delete_story_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!("Are you sure you want to delete this story? [Y/n]:");
+    let confirm = get_user_input();
+    confirm == "Y"
 }
 
 fn update_status_prompt() -> Option<Status> {
-    todo!();
+    println!("----------------------------");
+    println!("New Status (1 - OPEN, 2 - IN-PROGRESS, 3 - RESOLVED, 4 - CLOSED):");
+    let new_status = get_user_input();
+
+    match new_status.as_str() {
+        "1" => Some(Status::Open),
+        "2" => Some(Status::InProgress),
+        "3" => Some(Status::Resolved),
+        "4" => Some(Status::Closed),
+        _ => None,
+    }
 }
